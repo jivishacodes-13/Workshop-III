d1d9ce1a8e481642653c223f7f038bda7e8efeb3
jivishacodes-13 <jivishagupta07@gmail.com>
2026-07-31 11:24:21 +0530

    day2

---
+++ file.js
@@ -0,0 +1,20 @@
+console.log("======= Variables =========")
+var text = "JIVISHA";
+
+console.log(text)
+
+console.log("=========If Else==========")
+var a = 40 ; 
+var b = 20 ; 
+if (a>b){
+    console.log("A IS GREATER");
+}
+else{
+    console.log("B IS GREATER");
+}
+
+console.log("=========FUNCTIONS===========");
+function abes(){
+    console.log("THIS IS OUTPUT ABES OKAY ")
+}
+abes();
