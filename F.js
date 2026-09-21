d7f2ca9a9d0c7408bb5c9a94586b6452b3c4cf76
jivishacodes-13 <jivishagupta07@gmail.com>
2026-09-21 16:04:18 +0530

    timeout

--- F.js
+++ F.js
@@ -1,28 +1,34 @@
-class Employee {
-	constructor(id, name, basicSalary) {
-		this.id = id;
-		this.name = name;
-		this.basicSalary = basicSalary;
-	}
+// class Employee {
+// 	constructor(id, name, basicSalary) {
+// 		this.id = id;
+// 		this.name = name;
+// 		this.basicSalary = basicSalary;
+// 	}
 
-	calculateSalary() {
-		return this.basicSalary;
-	}
+// 	calculateSalary() {
+// 		return this.basicSalary;
+// 	}
+// }
+
+// class Manager extends Employee {
+// 	constructor(id, name, basicSalary, incentive) {
+// 		super(id, name, basicSalary);
+// 		this.incentive = incentive;
+// 	}
+
+// 	calculateSalary() {
+// 		return super.calculateSalary() + this.incentive;
+// 	}
+// }
+
+// const employee = new Employee(101, "Jivishavashava", 30000);
+// const manager = new Manager(102, "Omikavakavaka", 50000, 10000);
+
+// console.log("Employee Salary:", employee.calculateSalary());
+// console.log("Manager Salary:", manager.calculateSalary());
+// USING TIMEOUT PRINT SOMETHING AFTER 5 SECONDS
+function printMessage() {
+    console.log("Hello, jivishavashava.");
 }
-
-class Manager extends Employee {
-	constructor(id, name, basicSalary, incentive) {
-		super(id, name, basicSalary);
-		this.incentive = incentive;
-	}
-
-	calculateSalary() {
-		return super.calculateSalary() + this.incentive;
-	}
-}
-
-const employee = new Employee(101, "Jivishavashava", 30000000);
-const manager = new Manager(102, "Omikavakavaka", 500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 10000);
-
-console.log("Employee Salary:", employee.calculateSalary());
-console.log("Manager Salary:", manager.calculateSalary());
+setTimeout(printMessage, 5000);
+console.log("how are you????????");
